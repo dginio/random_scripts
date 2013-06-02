c6144c8f06a0f240a01760cfce06069df9f6c1e8
dginio <god@dginio.fr>
2013-06-02 15:01:44 +0200

    optimisations

--- conkygen.py
+++ conkygen.py
@@ -1,32 +1,46 @@
 #!/usr/bin/python
 #coding:utf-8
 
-# Generateur conky reseau dynamique - Antoine BOTTE
+# --- Dginio --- 
+
+# You need screen, scapy
+# The conkyrc file is written for a screen resolution of 1600*900
+
+# conkyrc generator, dynamic network informations
 import os,sys,urllib2
 from scapy.all import *
 
-if "0" in open("/home/dginio/scripts/exec_test","r").read() :
-	open("/home/dginio/scripts/exec_test","w").write("1")
+conf.verb = 0
 
-	conf.verb = 0
+# Execute sh command and return the result
+def execute(cmd): return subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE).stdout.read()
 
-	# Variables de fichiers utiles
-	pathscript = "/home/dginio/scripts/"
-	file_netinfo = pathscript+"netinfo.txt"
-	file_conky = pathscript+"conkyrc"
-	file_conkygen = pathscript+"conkygen.sh"
-	# configuration proxy pour scapy
-	# proxies = {'http': 'http://x.x.x.x:x'}
+# vars
+pathscript = "/home/user/scripts/conky/"
+current_filename = os.path.basename(__file__)
+file_proxy = pathscript+"../proxy/proxy.conf"
+file_netinfo = pathscript+"netinfo.txt"
+file_conky = pathscript+"conkyrc"
 
-	# Fonction d'execution de commande bash avec resultat en return
-	def execute(cmd): return subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE).stdout.read()
+# Uptade network informations every auto_refresh+10 seconds
+auto_refresh = "50"
 
-	# Recupère la résoultion actuelle
-	resolution = execute("echo $(xrandr | grep '\*\+' | egrep -o '[0-9]+x[0-9]+' )")
-	x = int(resolution.split("x")[0])
-	y = int(resolution.split("x")[1])
+# Check if this process is not already running
+if int(execute("ps aux | grep '"+current_filename+"' | grep -v 'grep' | wc -l").strip()) < 4 :
+	
+	# Get proxy informations from the proxyconf file
+	proxy = open(file_proxy,"r").read().split(" ")
+	
+	# proxy conf ( ip:port )
+	proxyPath = proxy[0].strip()
 
-	# Fonction de conversion d'un masque de sous réseau en bits => string
+	# state
+	if int(proxy[1].strip()) == 1 :
+		proxyState = "On"
+		proxies = {'http': 'http://'+proxyPath}
+	else : proxyState = "Off"
+	
+	# Convert netmask in bits ( return a string )
 	values=[128,64,32,16,8,4,2,1]
 	def netmaskToBits(mask):
 		byte=0
@@ -35,65 +49,76 @@ if "0" in open("/home/dginio/scripts/exec_test","r").read() :
 			for value in values:
 				if i >= value: byte += 1; i-= value
 		return str(byte)
+	
+	# 'write' means the conkyrc file must be rewrite
 	write = 0
 	if len(sys.argv) > 1 and sys.argv[1] == "write":
 		write = 1
-
+	
+	# Check if the connection is up
 	connect = 0
+	open_netinfo = open(file_netinfo,"r")
+	netinfo_content = open_netinfo.read()
+
 	try :
-		# Passerelle récupérée dans la liste des routes de scapy
+		# Get the gateway in scapy informations, if it works, we are connected
 		gw = [e[2] for e in conf.route.routes if e[2]!='0.0.0.0'][0]
 		connect = 1
-		if "Not Connected" in open(file_netinfo,'r').read():
+		# If we were not connected we need te rewrite the conkyrc file
+		if "Not Connected" in netinfo_content:
 			write = 1
 	except :
+		# The gateway can't be found, we are not connected
+		gw = ""
 		connect = 0
-	
+		# If we were connected we need to rewrite the conkyrc file
+		if not "Not Connected" in netinfo_content :
+			write = 1
+
+	open_netinfo.close()
+
 	if connect :
-		# Récupérer l'ip public
+		# Get the public IP
 		try :
 			request = urllib2.Request("http://www.dginio.free.fr/ip")
 			ip_pub = urllib2.urlopen(request).read()
 		except :
-			print "|"
 			ip_pub = "---"
 	
-		# Récupération de la table ARP
+		# ARP Table
 		arp = execute("arp -an")
 	
-		# Etat de l'association avec l'adresse MAC de la passerelle ( fixe ou dynamique )
+		# Check if the gateway is stuck in the ARP table
 		if "PERM" in arp: gwState = "Static"
 		else: gwState = "Dynamic"
 	
-		# Adresse mac de la passerelle depuis la table arp
+		# MAC address of the gateway in the ARP table
 		gwMac = re.compile(gw+'\) à (.*) \[').findall(arp)[0]
 	
-		# Adresse MAC de la passerelle attribué en cas de requete ARP ( detection MTIM )
+		# MAC address of the gateway with an ARP request
 		gwMacDyn = getmacbyip(gw)
 	
-		# Comparaison des adresse MAC pour détection MITM
+		# compare these address MAC to know if a potential ARP poisoning is running against us
 		if gwMac == gwMacDyn: mitm = "OK"
 		else: mitm = "${color3}MITM"
 	
-		net_iface = conf.iface
-	
-		# Ifconfig sur l'interface utilisée dans scapy
+		# ifconfig of the current interface
 		ifconfig = execute("ifconfig "+conf.iface)
 	
-		# Masque de sous réseau récupéré dans ifconfig
+		# get the netmask in the ifconfig
 		mask = re.compile('Masque:(.*)').findall(ifconfig)[0]
 	
-		# Masque de sous réseau pour le format cidr
+		# CIDR netmask
 		maskCidr = netmaskToBits(mask)
 	
-		# Adresse IP de l'interface récupéré dans ifconfig
+		# current ip address with ifconfig
 		addr = re.compile('inet adr:(.*) B').findall(ifconfig)[0][:-1]
 	
-		# Adresse mac de l'interface récupéré dans ifconfig
+		# current mac address with ifconfig
 		mac = re.compile('HWaddr (.*)').findall(ifconfig)[0]
 	
-		# Récupération de l'adresse du serveur DHCP (dans /var/lib/dhcp/dhclient.leases)
-		# Booléen permettant de définir si l'interface est passé dans la lecture du fichier lease DCHP
+		# get the DHCP server address in the file /var/lib/dhcp/dhclient.leases
+		# Loop on the file, if the current iface is found, the next DHCP server address is for us
 		findIface = 0
 		for line in re.compile('.*').findall(open('/var/lib/dhcp/dhclient.leases','r').read()):
 			if conf.iface in line: findIface = 1
@@ -101,52 +126,45 @@ if "0" in open("/home/dginio/scripts/exec_test","r").read() :
 				dhcp = re.compile('option dhcp-server-identifier (.*)').findall(line)[0][:-1]
 				findIface = 0
 	
-		# Adresse(s) IP de(s) serveur(s) DNS récupéré(s) dans le fichier /etc/resolv.conf
+		# get all DNS servers in the file /etc/resolv.conf
 		dns = ", ".join(re.compile('nameserver (.*)').findall(open('/etc/resolv.conf','r').read()))
-	
-		# Récupération des informations sur le proxy depuis le fichier de conf perso
-		proxy = open("/home/dginio/scripts/proxy/proxy.conf","r").read().split(" ")
-	
-		# Adresse:port du proxy
-		proxyPath = proxy[0].strip()
 
-		# Etat du proxy
-		if int(proxy[1].strip()) == 1: proxyState = "On"
-		else : proxyState = "Off"
-
-		# arping sur le réseau local avec scapy ajouté dans le fichier ips.txt
+		# arping on the local network > ips.txt
 		fips = open(pathscript+"ips.txt","w")
 		for ip in [t[1].psrc for t in arping(gw+"/"+maskCidr)[0][ARP]]: fips.write(ip+"\n")
 		fips.close()
 	
-		# Trie par IP le fichier ips.txt et supprime les doublons d'adresses
+		# sort and uniq on the file ips.txt
 		os.system("cat "+pathscript+"ips.txt |sort -n -t . -k 3,3n -k 4,4n |uniq > "+pathscript+"tmp ; mv "+pathscript+"tmp "+pathscript+"ips.txt")
 	
-		# Lecture du fichier ips.txt
+		# read the file ips.txt
 		fips = open(pathscript+"ips.txt","r").readlines()
 	
-		# Adresses placées dans un tableau sans les \n
+		# each ip address is added in the array ips
 		ips = []
 		for ip in fips: ips.append(ip.strip())
 	
-		# Envoi d'une requete ICMP à chaque entrées du tableau
+		# ICMP request for each address in the array ips
 		ans,unans=sr(IP(dst=ips)/ICMP(),timeout=1)
 	
-		# Récupération des réponses dans un dictionnaires reply avec les ttl
+		# reply store in a dict with ttl
 		reply = [(p[1].src,p[1].ttl) for p in ans[IP]]
 	
-		# Suppression des adresses IP qui n'ont pas répondu au ping en passant dans un fichier tmp
-		# fichier tmp remplacer par ips.txt ensuite pour conserver les ip actives
+		# keep only ip address which replied to the ping and write it in a tmp file
 		noreply = []
 		tmp = open(pathscript+"tmp","w")
 		for line in fips:
 			test = 0
 			for ip in [p.dst for p in unans[IP]]:	
-				if ip in line: test = 1
-			if test == 1: noreply.append(ip)
-			else: tmp.write(line)
+				if ip in line : test = 1
+			if test : noreply.append(ip)
+			else : tmp.write(line)
 		tmp.close()
+		
+		# the file tmp replace ips.txt to keep only active ip address for the next scan
 		os.system("mv "+pathscript+"tmp "+pathscript+"ips.txt")
+		
+		# building of the net var, it will contains all the network informations separated with a '&'
 		net = conf.iface+"&"+addr+"/"+maskCidr+"&"+mac+"&"+gw+" - "+gwState+" - "+mitm+"&"
 		if mitm == "OK": net += gwMac+"&"
 		else: gwMac+" | "+gwMacDyn+"&"
@@ -155,20 +173,23 @@ if "0" in open("/home/dginio/scripts/exec_test","r").read() :
 		for ip in noreply : net += " "*60+"   [-]  "+ip+" "*(17-len(ip))+"?\n"
 	
 	else :
+		# if we were not connected, the net var contains some empty fields and 'Not connected ?' instead of the iface
 		net = "Not connected ?"+"&"*8
+		conf.iface = ""
+	
+	open_netinfo = open(file_netinfo,"w")
+	open_netinfo.write(net)
+	open_netinfo.close()
 
-	tmp = open(pathscript+"tmp","w")
-	tmp.write(net)
-	tmp.close()
-	os.system("mv "+pathscript+"tmp "+file_netinfo)
-
-	if write == 1:
-		# Déclaration variables conky
+	# if we need to rewrite the conkyrc
+	if write :
+		# conky vars
 		goto = "100"
 		bar = "270"
 		refresh = "20"
+		# titles
 		t = ["SYSTEM","TOP","NETWORK","PUBLIC","HOSTS"]
-		def title(n) : return "${voffset 7}${font DroidSans:bold:size=8.25}${color3}"+t[n]+"${offset 8}${color0}${voffset -1}${cpubar cpu0 1,"+str(374-len(t[n]*5))+"}"
+		def title(n) : return "${voffset 7}${font DroidSans:bold:size=8.25}${color3}"+t[n]+"${offset 8}${color0}${voffset -1}${cpubar cpu0 1,"+str(374-len(t[n]*6))+"}"
 
 		nb_proc = int(execute("cat /proc/cpuinfo | grep processor | wc -l"))
 		cpu = []
@@ -176,7 +197,7 @@ if "0" in open("/home/dginio/scripts/exec_test","r").read() :
 			if i < nb_proc : cpu.append("${color1}CP"+str(i)+" :$color0 ${cpu cpu"+str(i)+"}% ${goto "+goto+"}$color3${cpubar cpu"+str(i)+" 6,"+bar+"}\n")
 			else : cpu.append("${color1}CP"+str(i)+" :$color0 none\n")
 
-		# Réécriture du fichier conkyrc
+		# let's go
 		conky = open(file_conky,'w')
 		conky.writelines([
 		"own_window yes\n",
@@ -231,7 +252,6 @@ if "0" in open("/home/dginio/scripts/exec_test","r").read() :
 		"${color1}${top name 7} ${color0}${top pid 7} ${top cpu 7} ${top mem 7}\n",
 		"${color1}${top name 8} ${color0}${top pid 8} ${top cpu 8} ${top mem 8}\n",
 		"${color1}${top name 9} ${color0}${top pid 9} ${top cpu 9} ${top mem 9}\n",
-		"${color1}${top name 10} ${color0}${top pid 10} ${top cpu 10} ${top mem 10}\n",
 		title(2)+"${font}\n",
 		"${color1}iface : ${color0}${texeci "+refresh+" cat "+file_netinfo+" | head -n 1 | cut -d'&' -f1 }\n",
 		"${color1}IP  : ${color0}${texeci "+refresh+" cat "+file_netinfo+" | head -n 1 | cut -d'&' -f2 }\n",
@@ -241,20 +261,20 @@ if "0" in open("/home/dginio/scripts/exec_test","r").read() :
 		"${color1}DHCP server : ${color0}${texeci "+refresh+" cat "+file_netinfo+" | head -n 1 | cut -d'&' -f6 }\n",
 		"${color1}DNS server(s) : ${color0}${texeci "+refresh+" cat "+file_netinfo+" | head -n 1 | cut -d'&' -f7 }\n",
 		"${color1}Proxy : ${color0}${texeci "+refresh+" cat "+file_netinfo+" | head -n 1 | cut -d'&' -f8 }\n",
-		"${color1}Downloaded : ${color0}${totaldown "+net_iface+"}\n",
-		"${color1}Uploaded   : ${color0}${totalup "+net_iface+"}\n",
-		"${color1}Download ${color0}${downspeed "+net_iface+"}\n",
-		"${color2}${downspeedgraph "+net_iface+" 25, 330 FFB515 D70303 -t}\n",
-		"${color1}Upload ${color0}${upspeed "+net_iface+"}\n",
-		"${color2}${upspeedgraph "+net_iface+" 25, 330 FFB515 D70303 -t}\n",
+		"${color1}Downloaded : ${color0}${totaldown "+conf.iface+"}\n",
+		"${color1}Uploaded   : ${color0}${totalup "+conf.iface+"}\n",
+		"${color1}Download ${color0}${downspeed "+conf.iface+"}\n",
+		"${color2}${downspeedgraph "+conf.iface+" 25, 330 FFB515 D70303 -t}\n",
+		"${color1}Upload ${color0}${upspeed "+conf.iface+"}\n",
+		"${color2}${upspeedgraph "+conf.iface+" 25, 330 FFB515 D70303 -t}\n",
 		title(3)+"${font}\n",
 		"${color1}${font :pixelsize=34} ${texeci "+refresh+" cat "+file_netinfo+" | head -n 1 | cut -d'&' -f9 }${font}\n",
-		"${offset 398}${voffset -800}"+title(4)+"${font}\n",
+		"${offset 398}${voffset -781}"+title(4)+"${font}\n",
 		"${color1}${texeci "+refresh+" cat "+file_netinfo+" | head -n 1 | cut -d'&' -f10 }\n",
-		"${color0}${texeci "+refresh+" cat "+file_netinfo+" | cut -d'&' -f11 }\n",
-		"${texeci 60 sudo bash "+file_conkygen+"}\n"
+		"${color0}${texeci "+refresh+" cat "+file_netinfo+" | cut -d'&' -f11 }\n"
+		"${texeci "+auto_refresh+" sleep 10 && sudo "+pathscript+current_filename+"}\n"
 		])
 		conky.close()
-
-	if not (execute("ps aux | grep 'conky -c' | grep -v grep")): execute("conky -c /home/dginio/scripts/conkyrc > /dev/null 2>&1")
-	open("/home/dginio/scripts/exec_test","w").write("0")
+	
+	# If conky us not running, this script will do that for you in a screen
+	if not (execute("ps aux | grep 'conky -c' | grep -v grep")): execute("screen -dmS conky conky -c "+file_conky)
